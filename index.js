71ab70cec292f078a5dbb249c686885912665926
ZaharLevish92 <levishzahar1992@gmail.com>
2023-04-06 22:29:23 +0300

    Made Modal Form
    HTML and CSS done

---
+++ index.js
@@ -0,0 +1,36 @@
+const refs = {
+    openModalBtn: document.querySelector("[data-open-modal]"),
+    closeModalBtn: document.querySelector("[data-close-modal]"),
+
+    backdrop: document.querySelector("[data-backdrop]"),
+  };
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  refs.backdrop.addEventListener("click", logBackDropClick);
+
+  function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+  function logBackDropClick() {
+    console.log("Это клик в бекдроп");
+  }
+</script>
+<script>
+  const refs = {
+    openModalBtn: document.querySelector("[data-open-modal]"),
+    closeModalBtn: document.querySelector("[data-close-modal]"),
+    closeModalBtn: document.querySelector("[data-close-modal]"),
+    backdrop: document.querySelector("[data-backdrop]"),
+  };
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  refs.backdrop.addEventListener("click", logBackDropClick);
+
+  function toggleModal() {
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+  function logBackDropClick() {
+    console.log("Это клик в бекдроп");
+  }
