12b824d29b47829c1246e477dd8f818027da0b2c
ZaharLevish92 <levishzahar1992@gmail.com>
2023-06-10 22:06:21 +0300

    LastCommitAdaptive
    AdaptiveDid

--- JS/indexFormOpen.js
+++ JS/indexFormOpen.js
@@ -1,35 +1,20 @@
-const refs = {
-  openModalBtn: document.querySelector("[data-open-modal]"),
-  closeModalBtn: document.querySelector("[data-close-modal]"),
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-open-modal]"),
+    closeModalBtn: document.querySelector("[data-close-modal]"),
 
-  backdrop: document.querySelector("[data-backdrop]"),
-};
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
+    backdrop: document.querySelector("[data-backdrop]"),
+  };
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
-refs.backdrop.addEventListener("click", logBackDropClick);
+  refs.backdrop.addEventListener("click", logBackDropClick);
 
-function toggleModal() {
-  refs.backdrop.classList.toggle("is-hidden");
-}
-function logBackDropClick() {
-  console.log("Это клик в бекдроп");
-}
-
-const refs = {
-  openModalBtn: document.querySelector("[data-open-modal]"),
-  closeModalBtn: document.querySelector("[data-close-modal]"),
-  closeModalBtn: document.querySelector("[data-close-modal]"),
-  backdrop: document.querySelector("[data-backdrop]"),
-};
-refs.openModalBtn.addEventListener("click", toggleModal);
-refs.closeModalBtn.addEventListener("click", toggleModal);
-
-refs.backdrop.addEventListener("click", logBackDropClick);
-
-function toggleModal() {
-  refs.backdrop.classList.toggle("is-hidden");
-}
-function logBackDropClick() {
-  console.log("Это клик в бекдроп");
-}
+  function toggleModal() {
+    document.body.classList.toggle ("modal-open")
+    refs.backdrop.classList.toggle("is-hidden");
+  }
+  function logBackDropClick() {
+    console.log("Это клик в бекдроп");
+  }
+})();
